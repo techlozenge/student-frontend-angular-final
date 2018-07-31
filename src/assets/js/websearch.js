f211e7c5abfdd16e14dc1a8470752357db900609
Daniel Ford <daniel.ford@libertyMutual.com>
2018-07-31 12:27:22 -0400

    misc updates

--- src/assets/js/websearch.js
+++ src/assets/js/websearch.js
@@ -4,14 +4,13 @@
         /* 
          * Search button click performs a Google search
          */
-
         $('#navbar').on('click', "#srchButton", function(e) {
             window.open("http://www.google.com/search?q=" + ($("#srchText").val()).split(' ').join('+'), "Search");
             console.log("performing a web search");
         });
 
         /* 
-         * If the user pressed the enter key will will throw the search button click
+         * If the user pressed the enter key this will throw the search button click
          */
         $('#navbar').keypress(function(e){
             if(e.which == 13){
