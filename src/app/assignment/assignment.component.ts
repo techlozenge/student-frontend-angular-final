8835b54c3b5b5be404991f0c6353d8c6b802cf20
Daniel Ford <daniel.ford@libertyMutual.com>
2019-10-25 15:57:08 -0400

    converting components to mat-table

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { fadeInAnimation } from '../animations/fade-in.animation';
-
 import { DataService } from '../data.service'
 import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
+import { fadeInAnimation } from '../animations/fade-in.animation';
+import { MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-assignment',
@@ -11,42 +11,72 @@ import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.compone
   styleUrls: ['./assignment.component.css'],
   animations: [fadeInAnimation]
 })
-export class AssignmentComponent implements OnInit {
+export class AssignmentComponent implements OnInit, AfterViewInit {
 
+  events: any[];
   errorMessage: string;
   successMessage: string;
-  assignments: any[];
   mode = 'Observable';
 
-  constructor (private dataService: DataService, public dialog: MatDialog) {}
- 
-  ngOnInit() { this.getAssignments(); }
- 
-  getAssignments() {
-    this.dataService.getRecords("assignment")
-      .subscribe(
-        assignments => this.assignments = assignments,
-        error =>  this.errorMessage = <any>error);  
+  // tslint:disable-next-line:max-line-length
+  displayedColumns = ['assignment_id', 'student_last_name', 'student_first_name', 'assignment_nbr', 'grade', 'subject', 'course', 'edit', 'delete'];
+
+  assignments: any;
+  dataSource: any;
+
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatSort;
+
+  ngOnInit() {
+    console.log('in OnInit');
   }
 
-  deleteAssignment(id:number, str:string) {
+constructor (private dataService: DataService, public dialog: MatDialog) {
+  console.log('in constructor');
+  this.getAssignments();
+}
 
-    let dialogRef = this.dialog.open(DeleteConfirmComponent, {
+  ngAfterViewInit(): void {
+    // console.log(' in ngAfterViewInit');
+  }
+
+  onRowClicked(row: any) {
+    // console.log('Row clicked: ', row);
+  }
+
+  onSortData(event: any) {
+    // console.log('event: ', event);
+  }
+
+  getAssignments() {
+    this.dataService.getRecords('assignment')
+      .subscribe(
+        assignments => {
+          console.log('in getassignments');
+          this.dataSource = new MatTableDataSource(assignments);
+          console.log('', this.dataSource.data);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          return this.assignments = assignments;
+        },
+        error => this.errorMessage = <any>error
+        )
+  }
+
+  deleteAssignment(id: number, str: string) {
+    const dialogRef = this.dialog.open(DeleteConfirmComponent, {
       data: {
         dataKey: id,
         value: str
       }
     });
-
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        this.dataService.deleteRecord("assignment", id)
+      if (result) {
+        this.dataService.deleteRecord('assignment', id)
           .subscribe(
-            assignment => {this.successMessage = "Record(s) deleted succesfully"; this.getAssignments(); },
+            assignment => {this.successMessage = 'Record(s) deleted succesfully'; this.getAssignments(); },
             error =>  this.errorMessage = <any>error);
       }
     });
   }
-
 }
-
