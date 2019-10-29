f965e6994141e5e2769e514824439e5dae0c9eb3
Daniel Ford <daniel.ford@libertyMutual.com>
2019-10-29 13:54:22 -0400

    converting components to mat-table

--- src/app/instructor/instructor.component.ts
+++ src/app/instructor/instructor.component.ts
@@ -1,52 +1,81 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-
 import { DataService } from '../data.service'
 import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
 import { fadeInAnimation } from '../animations/fade-in.animation';
+import { MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-instructor',
   templateUrl: './instructor.component.html',
   styleUrls: ['./instructor.component.css'],
-  animations: [fadeInAnimation],
+  animations: [fadeInAnimation]
 })
-export class InstructorComponent implements OnInit {
+export class InstructorComponent implements OnInit, AfterViewInit {
 
+  events: any[];
   errorMessage: string;
   successMessage: string;
-  instructors: any[];
   mode = 'Observable';
- 
-  constructor (private dataService: DataService, public dialog: MatDialog) {}
- 
-  ngOnInit() { this.getInstructor(); }
- 
-  getInstructor() {
-    this.dataService.getRecords("instructor")
-      .subscribe(
-        instructors => this.instructors = instructors,
-        error =>  this.errorMessage = <any>error);
+
+  displayedColumns = ['instructor_id', 'last_name', 'first_name', 'major', 'years_of_experience', 'tenured', 'edit', 'delete'];
+
+  instructors: any;
+  dataSource: any;
+
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatSort;
+
+  ngOnInit() {
+    console.log('in OnInit');
   }
 
-  deleteInstructor(id:number, str:string) {
+constructor (private dataService: DataService, public dialog: MatDialog) {
+  console.log('in constructor');
+  this.getInstructors();
+}
 
-    let dialogRef = this.dialog.open(DeleteConfirmComponent, {
+  ngAfterViewInit(): void {
+    // console.log(' in ngAfterViewInit');
+  }
+
+  onRowClicked(row: any) {
+    // console.log('Row clicked: ', row);
+  }
+
+  onSortData(event: any) {
+    // console.log('event: ', event);
+  }
+
+  getInstructors() {
+    this.dataService.getRecords('instructor')
+      .subscribe(
+        instructors => {
+          console.log('in getInstructors');
+          this.dataSource = new MatTableDataSource(instructors);
+          console.log('', this.dataSource.data);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          return this.instructors = instructors;
+        },
+        error => this.errorMessage = <any>error
+        )
+  }
+
+  deleteInstructor(id: number, str: string) {
+    const dialogRef = this.dialog.open(DeleteConfirmComponent, {
       data: {
         dataKey: id,
         value: str
       }
     });
-
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        this.dataService.deleteRecord("instructor", id)
+      if (result) {
+        this.dataService.deleteRecord('instructor', id)
           .subscribe(
-            instructor => {this.successMessage = "Record(s) deleted succesfully"; this.getInstructor(); },
-             error =>  (this.errorMessage = '***** POSSIBLE FOREIGN KEY CONSTRAINT ******     ' + <any>error));
-            //error =>  {(this.errorMessage = 'test'), console.log(this.errorMessage) });
+            instructor => {this.successMessage = 'Record(s) deleted succesfully'; this.getInstructors(); },
+            error =>  this.errorMessage = <any>error);
       }
     });
   }
-
 }
--- src/app/major/major.component.ts
+++ src/app/major/major.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { fadeInAnimation } from '../animations/fade-in.animation';
-
 import { DataService } from '../data.service'
 import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
+import { fadeInAnimation } from '../animations/fade-in.animation';
+import { MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-major',
@@ -11,41 +11,71 @@ import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.compone
   styleUrls: ['./major.component.css'],
   animations: [fadeInAnimation]
 })
-export class MajorComponent implements OnInit {
+export class MajorComponent implements OnInit, AfterViewInit {
 
+  events: any[];
   errorMessage: string;
   successMessage: string;
-  majors: any[];
   mode = 'Observable';
- 
-  constructor (private dataService: DataService, public dialog: MatDialog) {}
- 
-  ngOnInit() { this.getMajors(); }
- 
-  getMajors() {
-    this.dataService.getRecords("major")
-      .subscribe(
-        majors => this.majors = majors,
-        error =>  this.errorMessage = <any>error);
+
+  displayedColumns = ['major_id', 'major', 'edit', 'delete'];
+
+  majors: any;
+  dataSource: any;
+
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatSort;
+
+  ngOnInit() {
+    console.log('in OnInit');
   }
 
-  deleteMajor(id:number, str:string) {
-
-    let dialogRef = this.dialog.open(DeleteConfirmComponent, {
-      data: {
-        dataKey: id,
-        value: str
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        this.dataService.deleteRecord("major", id)
-          .subscribe(
-            major => {this.successMessage = "Record(s) deleted succesfully"; this.getMajors(); },
-            error =>  this.errorMessage = <any>error);
-      }
-    });
+  constructor (private dataService: DataService, public dialog: MatDialog) {
+    console.log('in constructor');
+    this.getMajors();
   }
 
+    ngAfterViewInit(): void {
+      // console.log(' in ngAfterViewInit');
+    }
+
+    onRowClicked(row: any) {
+      // console.log('Row clicked: ', row);
+    }
+
+    onSortData(event: any) {
+      // console.log('event: ', event);
+    }
+
+    getMajors() {
+      this.dataService.getRecords('major')
+        .subscribe(
+          majors => {
+            console.log('in getMajors');
+            this.dataSource = new MatTableDataSource(majors);
+            console.log('', this.dataSource.data);
+            this.dataSource.sort = this.sort;
+            this.dataSource.paginator = this.paginator;
+            return this.majors = majors;
+          },
+          error => this.errorMessage = <any>error
+          )
+    }
+
+    deleteMajor(id: number, str: string) {
+      const dialogRef = this.dialog.open(DeleteConfirmComponent, {
+        data: {
+          dataKey: id,
+          value: str
+        }
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.dataService.deleteRecord('major', id)
+            .subscribe(
+              major => {this.successMessage = 'Record(s) deleted succesfully'; this.getMajors(); },
+              error =>  this.errorMessage = <any>error);
+        }
+      });
+    }
 }
