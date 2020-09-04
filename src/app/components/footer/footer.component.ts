375ffc50cb4d83ca0536332bbdba779753c276e5
Daniel Ford <daniel.ford@libertyMutual.com>
2020-09-04 12:15:13 -0400

    clippyjs

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import clippy from 'clippyjs';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private static AGENT_NAME = new Array('Merlin', 'Links', 'Genius', 'Genie', 'Clippy', 'F1', 'Rocky', 'Rover', 'Peedy', 'Bonzi');
+
   constructor() { }
 
   ngOnInit() {
+    this.loadClippy();
+  }
+
+   loadClippy() {
+        clippy.load(this.randomAgent(), (agent) => {
+            agent.show();
+            agent.animate();
+        });
+    }
+
+    randomAgent(): string {
+      const randomNum = Math.floor(Math.random() * FooterComponent.AGENT_NAME.length);
+      return FooterComponent.AGENT_NAME[randomNum];
   }
 
 }
